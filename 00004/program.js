6a5e6613ad06536ffd09e9d67a8864cb4b950f15
Karn Ratana (Laptop) <karn@ratana.us>
2012-01-25 23:18:25 -0500

    00004 - added comments

--- 00004/program.js
+++ 00004/program.js
@@ -12,6 +12,10 @@ Ratana.Karn.Euler.P00004.prototype.run = function () {
     var n1 = threshhold2;
     var n2 = threshhold2;
     
+    // calculating backward from 999;
+    // we can improve this by cutting the step short
+    // once we find the largest palindrome.
+    // no need to calculate the smaller numbers
     var counter = 0;
     while (counter < (threshhold2 - threshhold1)) { 
         var i2 = threshhold2 - counter;
