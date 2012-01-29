b4a44fd19444e1a35b8a5e0cc011a5efa0737cf5
Karn Ratana (Laptop) <karn@ratana.us>
2012-01-28 23:34:00 -0500

    00006 - Completed

---
+++ 00006/program.js
@@ -0,0 +1,29 @@
+var Ratana = {};
+Ratana.Karn = {};
+Ratana.Karn.Euler = {};
+Ratana.Karn.Euler.P00006 = function () {}
+Ratana.Karn.Euler.P00006.prototype.run = function (input) {
+    return this.squareOfSum(input) - this.sumOfSquare(input);
+}
+Ratana.Karn.Euler.P00006.prototype.squareOfSum = function (input) {
+    var result = 0;
+    for (var ii = 1; ii <= input; ii++) {
+        result += ii;
+    }
+    result = result * result;
+    //console.log("squareOfSum = "+ result);
+    return result;
+}
+Ratana.Karn.Euler.P00006.prototype.sumOfSquare = function (input) {
+    var result = 0;
+    for (var ii = 1; ii <= input; ii++) {
+        result += ii * ii;
+    }
+    //console.log("sumOfSquare = "+ result);
+    return result;
+}
+
+
+var program = new Ratana.Karn.Euler.P00006();
+var result = program.run(100);
+console.log(result);
