a96ccc5cc85ff0435a9e7a17a1cabc73431c787f
Karn Ratana <karn.ratana@gmail.com>
2012-02-23 22:27:08 -0500

    00007 - Added problem

---
+++ 00007/program.js
@@ -0,0 +1,12 @@
+var Ratana = {};
+Ratana.Karn = {};
+Ratana.Karn.Euler = {};
+Ratana.Karn.Euler.P00007 = function () {}
+Ratana.Karn.Euler.P00007.prototype.run = function () {
+    return '__TODO__';
+}
+
+
+var program = new Ratana.Karn.Euler.P00007();
+var result = program.run();
+console.log(result);
