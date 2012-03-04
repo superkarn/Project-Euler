70834d74917cd8e610685245e1826a53d6ce8f32
Karn Ratana <karn.ratana@gmail.com>
2012-03-03 23:12:08 -0500

    00010 - Completed

--- 00010/program.js
+++ 00010/program.js
@@ -2,11 +2,26 @@ var Ratana = {};
 Ratana.Karn = {};
 Ratana.Karn.Euler = {};
 Ratana.Karn.Euler.P00010 = function () {}
-Ratana.Karn.Euler.P00010.prototype.run = function () {
-    return '__TODO__';
+Ratana.Karn.Euler.P00010.prototype.isPrime = function (input) {
+    // copied from P00007
+    // return false if input has at least one factor (not 1 or itself)
+    for (var ii = 2; ii <= Math.sqrt(input); ii++) {
+        if (input % ii == 0) return false;
+    }
+    
+    // if no factor, it's a prime
+    return true;
+}
+Ratana.Karn.Euler.P00010.prototype.run = function (input) {
+    var result = 0;
+    
+    for (var ii = 2; ii <= input; ii++) {
+        if (this.isPrime(ii)) result += ii;
+    }
+    return result;
 }
 
 
 var program = new Ratana.Karn.Euler.P00010();
-var result = program.run();
+var result = program.run(2000000);
 console.log(result);
