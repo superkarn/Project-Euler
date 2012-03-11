65fd3e96eecb9a3ec61f3b91d5bc0ca22737c5fd
Karn Ratana <karn.ratana@gmail.com>
2012-03-11 17:20:20 -0400

    00016 - Added problem

---
+++ 00016/program.js
@@ -0,0 +1,11 @@
+var Ratana = {};
+Ratana.Karn = {};
+Ratana.Karn.Euler = {};
+Ratana.Karn.Euler.P00016 = function () {}
+Ratana.Karn.Euler.P00016.prototype.run = function (input) {
+    return "__TODO__";
+}
+
+var program = new Ratana.Karn.Euler.P00016();
+var result = program.run(1000);
+console.log(result);
