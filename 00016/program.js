a4d280a248e4d488f383470cdebc2eddfb48c82b
Karn Ratana <karn.ratana@gmail.com>
2012-03-17 17:23:53 -0400

    00016 - Completed

--- 00016/program.js
+++ 00016/program.js
@@ -1,9 +1,24 @@
-var Ratana = {};
-Ratana.Karn = {};
-Ratana.Karn.Euler = {};
+var Ratana = Ratana || {};
+Ratana.Karn = Ratana.Karn || {};
+Ratana.Karn.Euler = Ratana.Karn.Euler || {};
 Ratana.Karn.Euler.P00016 = function () {}
 Ratana.Karn.Euler.P00016.prototype.run = function (input) {
-    return "__TODO__";
+    var BigNumber = Ratana.Karn.Euler.Library.BigNumber;
+    
+    var result = 0;
+    
+    // calculate 2^input (there's a better way to calculate this)
+    var powerOf2 = new BigNumber(1);
+    for (var ii=1; ii<=input; ii++) {
+        powerOf2 = BigNumber.multiply(powerOf2, "2");
+    }
+
+    // sum the digits
+    for (var ii in powerOf2.Value) {
+        result += powerOf2.Value[ii];
+    }
+    
+    return result;
 }
 
 var program = new Ratana.Karn.Euler.P00016();
